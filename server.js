0f333002b30baa6cb9edf355edc327395079a4ef
Glitch (great-blouse) <none>
2020-10-21 05:56:40 +0000

    🐂🛋 Updated with Glitch

--- server.js
+++ server.js
@@ -11,10 +11,6 @@ var app = express();
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
